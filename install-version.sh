2dd1cc46b77fa4a2226636825254c24b6157a49d
Mike Nolta <mike@nolta.net>
2025-01-23 09:49:41 -0500

    install.sh: rename install-version; don't install compiler,repos; fewer externals

--- install-version.sh
+++ install-version.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -eu
 
 spack_version=${1:-v0.23}
 
@@ -6,7 +7,6 @@ spack_version=${1:-v0.23}
 # https://spack.readthedocs.io/en/latest/getting_started.html
 sudo dnf install -y \
 	bzip2 \
-	gcc-toolset-13 \
 	git \
 	patch \
 	python3 \
@@ -21,18 +21,11 @@ git clone -c feature.manyFiles=true --depth=1 \
 # setup spack environment
 . $spack_version/share/spack/setup-env.sh
 
-# configure compiler
-(
-	. /opt/rh/gcc-toolset-13/enable
-	spack compiler find --scope=site
-)
-
 # use OS for basic and/or sensitive packages
 # https://github.com/NERSC/spack-infrastructure/blob/main/spack-externals.md
 {
 	sudo dnf install -y \
 		libcurl-devel \
-		m4 \
 		openssl-devel
 
 	spack external find --scope=site \
@@ -42,15 +35,9 @@ git clone -c feature.manyFiles=true --depth=1 \
 		diffutils \
 		findutils \
 		git \
-		m4 \
 		openssh \
 		openssl \
 		sed \
-		slurm \
-		tar \
-		unzip
+		tar
 }
-
-# add repos
-spack repo add repos/scinet
 
